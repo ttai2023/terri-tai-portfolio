bf3c9ec596e482574d9490d09fc9731b32a6ca20
ttai2023 <territai0831@gmail.com>
2026-05-22 15:02:58 -0700

    updated status

--- src/App.tsx
+++ src/App.tsx
@@ -599,6 +599,8 @@ const AboutPage: React.FC<{ soundEnabled: boolean }> = ({ soundEnabled }) => (
             <p className="flex items-start gap-2"><span className="text-slate-500">|-</span> ELC Tutor @ Jacobs School of Engineering UCSD.</p>
             <p className="flex items-start gap-2"><span className="text-slate-500">|-</span> Vice President Internal @ WIC UCSD.</p>
             <p className="flex items-start gap-2"><span className="text-slate-500">|-</span> Publicity Chair @ HKN UCSD.</p>
+            <p className="flex items-start gap-2"><span className="text-slate-500">|-</span> Vice President Internal @ WIC UCSD.</p>
+            <p className="flex items-start gap-2"><span className="text-slate-500">|-</span> Publicity Chair @ HKN UCSD.</p>
           </div>
           <p>&gt; Status: Incoming SDE Intern at Amazon AWS Summer 2026</p>
         </motion.div>
